d89567a45f93a2e72f8a3f361c18f9acdcae1f36
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2014-06-17 12:47:26 +0600

    add node methods

--- Neo4jRestAPI.py
+++ Neo4jRestAPI.py
@@ -24,6 +24,15 @@ class Neo4jRestAPI:
 		page = resp.read()
 		return page
 
+	def delete(self, url, headers):
+		opener = urllib2.build_opener(urllib2.HTTPHandler)
+		req = urllib2.Request(url, None, headers = headers)
+		req.get_method = lambda: "DELETE"
+		resp = opener.open(req)
+		page = resp.read()
+		return page
+
+	#cypher queries
 	def cypher_query(self, query, params):
 		headers = {
 			"Accept" : "application/json; charset=UTF-8",
@@ -33,14 +42,48 @@ class Neo4jRestAPI:
 			"query" : query,
 			"params" : params,
 		}
-		page = self.post('cypher', data = data, headers = headers)
+		page = self.post(url = 'cypher', data = data, headers = headers)
 		return json.loads(page)
 
+	#streaming
 	def streaming(self):
 		headers = {
 			"Accept" : "application/json; charset=UTF-8",
 			"X-Stream" : "true",
 		}
 		page = self.get(url = '', headers = headers)
+		return json.loads(page)
+
+	#get property keys
+	def propery_keys(self):
+		headers = {
+			"Accept" : "application/json; charset=UTF-8",
+		}
+		page = self.get(url = 'properykeys', headers = headers)
+		return json.loads(page)
+
+	#create node
+	def create_node(self, properties):
+		headers = {
+			"Accept" : "application/json; charset=UTF-8",
+			"Content-Type" : "application/json",
+		}
+		page = self.post(url = 'node', data = properties, headers = headers)
+		return json.loads(page)
+
+	#get node
+	def get_node(self, node_id):
+		headers = {
+			"Accept" : "application/json; charset=UTF-8",
+		}
+		page = self.get(url = 'node/' + str(node_id), headers = headers)
+		return json.loads(page)
+
+	#delete node
+	def delete_node(self, node_id):
+		headers = {
+			"Accept" : "application/json; charset=UTF-8",
+		}
+		page = self.delete(url = 'node/' + str(node_id), headers = headers)
 		return json.loads(page)
 
